53613b3fdf040b897264a36d2988335dd51804b4
jorgitom1980 <141953007+jorgitom1980@users.noreply.github.com>
2023-10-22 13:17:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// test data 
+// vanilla,vanilla,vanilla,strawberry,coffee,coffee
+
+// Prompt User for flavors 
+const flavors = prompt('Please enter your order (comma separated) ')
+
+// Split data into an array 
+const flavorArr = flavors.split(',')
+
+// Declare an empty object to hold flavor counts 
+const flavorCounts = {} 
+
+// forEach function using flavor 
+flavorArr.forEach(flavor => {
+    // check if flavor exists 
+    if (!flavorCounts[flavor]) {
+        // Initialize to zero 
+        flavorCounts[flavor] = 0 
+    }
+    // Add 1 to flavorCounts if flavor exists
+    flavorCounts[flavor]++ 
+}
+)
+// Print out data as a table 
+console.table(flavorCounts)
